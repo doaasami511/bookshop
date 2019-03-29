afef70b1bc27f908b388fcb08d6de8f5de26580b
asmaafarrag <48455830+asmaafarrag@users.noreply.github.com>
2019-03-29 23:14:28 +0200

    asmaa update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,110 @@ use App\Book;
 class PostController extends Controller
 {
     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::orderBy('created_at','asc')->paginate(2);
+        return view('books.index')->with('books',$books);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'author' => 'required',
+            'price' => 'required',
+            'count' => 'required',
+            'category' => 'required',
+        ]);
+
+        $book = new Book;
+        $book->title = $request->input('title');
+        $book->author = $request->input('author');
+        $book->price = $request->input('price');
+        $book->count = $request->input('count');
+        $book->category = $request->input('category');
+        $book->description = $request->input('description');
+        $book -> save();
+
+        return redirect('/books')->with('success', 'Book Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::find($id);
+        return view('books.show')->with('book',$book);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $Book = Book::find($id);
+        return view('books.edit')->with('book',$Book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'author' => 'required',
+            'price' => 'required',
+            'count' => 'required',
+            'category' => 'required',
+        ]);
+
+        $book = Book::find($id);
+        $book->title = $request->input('title');
+        $book->author = $request->input('author');
+        $book->price = $request->input('price');
+        $book->count = $request->input('count');
+        $book->category = $request->input('category');
+        $book->description = $request->input('description');
+        $book -> save();
+
+        return redirect('/books')->with('success', 'Book Updated');
+    }
+
+    /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
